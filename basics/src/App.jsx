e6c5fa8afb5da617d8fcfaff186b8244b91a50a4
yashgarg7302 <yashgarg7302@gmail.com>
2024-01-31 11:55:29 +0530

    bgcolor

--- basics/src/App.jsx
+++ basics/src/App.jsx
@@ -1,34 +1,33 @@
-import {useState} from 'react'
-import './App.css'
-
-
+import { useState } from "react"
 
 function App() {
-  
- let [counter, setCounter] = useState(15);
- 
-  const addValue = () => {
-    counter = counter + 1;
-    setCounter(counter)
-  }
 
-  const removeValue = () => {
-    counter = counter - 1;
-    setCounter(counter)
-  }
+  const [color, setColor] = useState("olive")
 
   return (
-    <>
-    <h1 class="text-black bg-green-400 text-5xl font-bold underline">REACT HOOKS</h1>
-    <h2>Counter value : {counter} </h2>
-    <button
-    onClick={addValue}
-    >Add value</button>
-    <br />
-    <button
-    onClick={removeValue}
-    > remove value</button>
-    </>
+    <div className="w-full h-screen duration-200"
+    style={{backgroundColor: color}}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-3xl">
+          <button
+          onClick={() => setColor("red")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "red"}}
+          >Red</button>
+          <button
+          onClick={() => setColor("green")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "green"}}
+          >Green</button>
+          <button
+          onClick={() => setColor("blue")}
+          className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+          style={{backgroundColor: "blue"}}
+          >Blue</button>
+        </div>
+      </div>
+    </div>
         
   )
 }
